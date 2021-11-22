554394e8c6fa075aa7a1882af411609d35b029de
imsaraswat <94692317+imsaraswat@users.noreply.github.com>
2021-11-22 18:31:31 +0530

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,214 @@
+# print ("harsh")
+# print ("harsh")
+# print ("harsh")
+# print ("harsh")
+# print ("\"harsh")
+# a=6;
+# b=9;
+# c="saraswat";
+# print (a+b)
+# print (a+b)
+# print (a+b)
+# print ("\n for new line")
+# print(c.islower())
+# print(c.isupper())
+# print(c.upper())
+# print(c[3])
+# print("number S A R A S W A T")
+# print("       0 1 2 3 4 5 6 7")
+# print(c.index("a"))
+# print(5+9)
+# print(pow(2,4))
+# n1 = input ("enter no 1 : ")
+# n2 = input ("enter no 2 : ")
+# add = n1+n2
+# add1 = int(n1)+int(n2)
+# sub = int(n1)-int(n2)
+# div =int(n1)/int(n2)
+# mul =int(n1)*int(n2)
+# print("addition: ",add1)
+# print("subtraction: ",sub)
+# print("Div :",div)
+# print("Mul :",mul)
+#
+# def myfunc(name2):
+#     print("my name is " +name2)
+# myfunc("harsh")
+#
+# myfunc("saraswat")
+# #if
+#
+# b = 33
+# a = 33
+# if b > a:
+#     print("b is greater than a")
+# elif a>b:
+#     print("a is greater than b")
+# else:
+#     print("both are equal")
+#     # for comment
+# a=int(input("Enter length value  "))
+# b=int(input("Enter breadth value  "))
+# if b==a:
+#     print("It is a square")
+# else:
+#     print("It is a rectangle")
+#
+# a=int(input("Enter value  "))
+#
+# if a%7==0:
+#     print("divisible by 7")
+# else:
+#     print("not divisable")
+
+# a=float(input("Hello how are you today! \nEnter numbers: "))
+# b=float(input("Enter numbers: "))
+#
+# print("select option from below \n 1. Add \n 2. Sub \n 3. Multiply \n 4. Divide\n")
+# c=int(input("Enter your choice: "))
+# if c==1:
+#     print("Addition: ",a+b)
+#
+# elif c==2:
+#     print("Subtraction: ",a-b)
+#
+# elif c==3:
+#     print("Multiplication: ",a*b)
+#
+# elif c==4:
+#     print("Divide: ",a/b)
+#
+# else:
+#     print("Invalid operator")
+
+# start, end = 1, 100
+# print("intigers from 1 to 100")
+# for num in range(start, end + 1):
+#     if num % 2 == 0:
+#           print(num, end=" ")
+
+# print("even numbers from 1 to 100 are")
+# i=1
+# while i<=100:
+#         if i%2==0:
+#            print(i)
+#         i=i+1
+
+# print("even umbers from 1 to 100 are")
+# i=1
+# while i<=100:
+#     print(i)
+#     i=i+2
+#     #i=i+2
+
+# word ="age"
+# guess=""
+# print("what goes up but never down")
+# count=0
+# limit=3
+# out_of_guesses = False
+# while guess != word and not(out_of_guesses):
+#     if count < limit:
+#         # guess= raw_input("Enter the guess: ").lower()
+#          guess = input("enter the guess: ")
+#         count +=1
+#     else:
+#         out_of_guesses=True
+# if out_of_guesses == True:
+#     print("out of guesses you lost")
+# else:
+#     print("you win")
+
+
+
+# i=10
+# while i<=300:
+#     if i%10==0:
+#         print(i)
+#     i=i+10
+
+
+# i=10
+# while i>=1:
+#     # if i%1==0:
+#     print(i)
+#     i=i-1
+
+# number = 10
+# total = 0
+#
+# for number in range(1, number+1):
+#
+#     if(number % 2 == 0):
+#         # print("{0}".format(number))
+#         total = total + number
+# print("The Sum of Even Numbers from 1 to 10= {1}".format(number, total))
+
+
+# i=105
+# while i>=7:
+#     print(i)
+#     i=i-7
+
+# number = int(input ("Enter the number of which you want table: "))
+# print ("The Table of: ",number)
+# for count in range(1, 11):
+#    print (number,'x',count,'=',number*count)
+
+
+# i=105
+# while i>=7:
+#      print(i, end=',')
+#      i=i-7
+
+
+#
+# name = ["vani", "vidula", "darsh"]
+# letter="harsh saraswat"
+# for x in letter:
+#     print(x)
+# for i in name:
+#         print(i)
+# for index in range(5,10):
+#     print(index)
+
+
+# for k in range(5)
+#     print(k)
+
+# for i in range(10,15)
+#     print(i, end=",")
+
+# name = ["harsh", "monika", "divij"]
+# for s in name[2:3]:
+#     print(s)
+
+
+
+# for i in range(1,11):
+# 	print(i)
+
+# for i in range(20,1,-2):
+# 	print(i, end=" ")
+
+# a=int(input("enter any number: "))
+# for i in range(1,11):
+# 	print(a," x ",i," = ", i*a)
+
+# a="harsh saraswat"
+# print("my name is : " +a)
+# print("my name is : " +a.upper())
+# print("my name is : " +a.lower())
+# print(a.islower())
+# print(a.isupper())
+# print(a.upper().isupper())
+# print(a.lower().islower())
+# print(len(a))
+# print(a[5])
+#
+#
+# import calendar
+# year =int( input("Enter the year of the required calendar "))
+# month = int( input("Enter the month of the required calendar "))
+# print(calendar.month(year,month))
+
